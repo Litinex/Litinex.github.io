654590d46cd683cac669dabf3ee1912866703512
Litinex <1279137906@qq.com>
2026-04-25 23:36:06 +0800

    Configure NetEase playlist for site player

--- music-config.js
+++ music-config.js
@@ -2,9 +2,9 @@ window.siteMusicConfig = {
   provider: "netease",
   type: "playlist",
   id: "",
-  shareUrl: "",
-  title: "My Playlist",
-  subtitle: "NetEase Cloud Music",
+  shareUrl: "https://music.163.com/playlist?id=13957431115&uct2=U2FsdGVkX1/qJtfy2bYOdcyz8+kXpqPzK+Z+mXVz4Yk=",
+  title: "叶子庄的歌单",
+  subtitle: "网易云音乐",
   autoPlay: false,
   startOpen: false,
 };
